ede422c7d87ed1fdccb5f3f235d6aee20f2e98b3
YTim <yun2193@hanmail.net>
2023-01-01 20:26:09 +0900

    react query 설치 & css reset 세팅
    reset: createGlobalStyle

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./App";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+);
