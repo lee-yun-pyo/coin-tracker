a01c7f2c3d3d7ff6cb46881555d4aad4994b814c
YTim <yun2193@hanmail.net>
2023-01-03 21:53:21 +0900

    코인 정보 API fetch 코드

--- src/Routes/Coin.tsx
+++ src/Routes/Coin.tsx
@@ -1,11 +1,44 @@
+import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
+import { fetchInfo, fetchPrice } from "../api";
 
 interface IcoinId {
   coinId: string;
 }
 
+interface Iinfo {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  logo: string;
+  tags: object;
+  team: object;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  links: object;
+  links_extended: object;
+  whitepaper: object;
+  first_data_at: string;
+  last_data_at: string;
+}
+
 function Coin() {
   const { coinId } = useParams<IcoinId>();
+  const { isLoading: infoLoading, data: infoData } = useQuery<Iinfo>(
+    "info",
+    () => fetchInfo(coinId)
+  );
   return <h1>{coinId}</h1>;
 }
 
